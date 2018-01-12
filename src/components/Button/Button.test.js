e1e2a0058641a6fb6662a07cae1b5d32d279eb49
Shane Jonas <jonas.shane@gmail.com>
2018-01-11 17:14:53 -0800

    problem: test broke with muiTheme wrapper solution: use `dive` to get at deeper element with enzyme

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -6,12 +6,12 @@ import { FlatButton } from 'material-ui';
 describe('Button', () => {
   it('shows a default button', () => {
     const button = shallow(<Button label="My Label"/>);
-    expect(button.find(FlatButton).length > 0).toEqual(true);
+    expect(button.find('Button').dive().find(FlatButton).length > 0).toEqual(true);
   });
 
   it('shows a primary button', () => {
-    const button = shallow(<Button primary label="My Label"/>);
-    expect(button.find(FlatButton).props().primary).toEqual(true);
+    const muiWrapper = shallow(<Button primary label="My Label"/>);
+    expect(muiWrapper.find('Button').dive().find(FlatButton).props().primary).toEqual(true);
   });
 });
 
