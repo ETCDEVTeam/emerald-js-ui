090735b9ce4db497f917fb9c562bd2245aeaeb41
gagarin55 <gagarin55@protonmail.com>
2018-02-27 15:03:43 +0300

    problem: show syncwarning only when not synced

--- src/components/SyncWarning/SyncWarning.js
+++ src/components/SyncWarning/SyncWarning.js
@@ -10,17 +10,21 @@ const SyncWarning = (props) => {
     classes, ...other
   } = props;
 
-  return (
-    <div className={classes.container}>
-      <div><Spinner color={styles.text.color} /></div>
-      <div className={classes.text}>
-                We are syncing, your balances can be wrong
-      </div>
-      { startingBlock && currentBlock && highestBlock && 
-        (<div className={classes.text}>
-            {startingBlock}/{currentBlock}/{highestBlock}
-        </div>)}
-    </div>);
+  const height = highestBlock || 5464306;
+  if (height - currentBlock >= 20) {
+    return (
+      <div className={classes.container}>
+        <div><Spinner color={styles.text.color} /></div>
+        <div className={classes.text}>
+                  You are connected to not synced node, your balances can show wrong value.
+        </div>
+        { startingBlock && currentBlock && highestBlock &&
+          (<div className={classes.text}>
+              About {highestBlock - currentBlock} blocks left.
+          </div>)}
+      </div>);
+  }
+  return null;
 };
 
 export default (injectSheet(styles)(SyncWarning));
