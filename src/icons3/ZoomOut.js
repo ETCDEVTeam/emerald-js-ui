072b16d062ad5101bcb8e32b2730bb72083ac382
gagarin55 <gagarin55@protonmail.com>
2018-05-03 09:35:32 +0300

    problem: default color for Buttons label should be _white_

--- src/icons3/ZoomOut.js
+++ src/icons3/ZoomOut.js
@@ -3,7 +3,8 @@ import SvgIcon from 'material-ui/SvgIcon';
 
 const ZoomOut = props => (
   <SvgIcon {...props} viewBox="0 0 64 64">
-    <circle cx="28" cy="28" r="20" fill="none" stroke="currentColor" strokeWidth="4" strokeMiterlimit="10"/><path fill="none" stroke="currentColor" d="M56 56L42.14 42.14M16 28h24" strokeWidth="4" strokeMiterlimit="10"/>
+    <circle cx="28" cy="28" r="20" fill="none" stroke="currentColor" strokeWidth="4" strokeMiterlimit="10" />
+    <path fill="none" stroke="currentColor" d="M56 56L42.14 42.14M16 28h24" strokeWidth="4" strokeMiterlimit="10" />
   </SvgIcon>
 );
 
