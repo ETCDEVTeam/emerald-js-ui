654febf524489bae31e8958f3d9531d1a4820a5d
Zachary Belford <belfordz66@gmail.com>
2018-06-19 13:44:14 -0700

    Problem: padding is random and inconsistent

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -11,45 +11,62 @@ function getStyles(muiTheme) {
   return {
     wrapper: {
       border: `1px solid ${muiTheme.palette.borderColor}`,
-      backgroundColor: muiTheme.palette.alternateTextColor
+      backgroundColor: muiTheme.palette.alternateTextColor,
     },
     toolbar: {
       background: 'transparent',
-      height: '100px'
-    }
-  }
+      height: '100px',
+    },
+    childWrapper: {
+      padding: '50px',
+    },
+  };
 }
+
+const getIconWithButton = (icon) => {
+  if (!icon) { return <div />; }
+  return (
+    <ToolbarGroup>
+      <IconButton>
+        {icon}
+      </IconButton>
+    </ToolbarGroup>
+  );
+};
 
 export class Page extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
-  }
+    muiTheme: PropTypes.object.isRequired,
+    children: PropTypes.element.isRequired,
+    rightIcon: PropTypes.element,
+    leftIcon: PropTypes.element,
+  };
 
-  getIconWithButton(icon) {
-    if (!icon) { return <div />; }
-    return (
-      <ToolbarGroup>
-        <IconButton>
-          {icon}
-        </IconButton>
-      </ToolbarGroup>
-    );
-  }
+  static defaultProps = {
+    rightIcon: null,
+    leftIcon: null,
+  };
 
   render() {
-    const {muiTheme, title, leftIcon, rightIcon} = this.props
+    const { muiTheme, title, leftIcon, rightIcon } = this.props;
     const styles = getStyles(muiTheme);
+
     return (
       <Card style={styles.wrapper}>
         <Toolbar style={styles.toolbar}>
-          {this.getIconWithButton(leftIcon)}
-          <ToolbarTitle style={{lineHeight: styles.toolbar.height}} text={title} />
-          {this.getIconWithButton(rightIcon)}
+          {getIconWithButton(leftIcon)}
+          <ToolbarTitle style={{ lineHeight: styles.toolbar.height }} text={title} />
+          {getIconWithButton(rightIcon)}
         </Toolbar>
+
         <Divider />
-        {this.props.children}
+
+        <div style={{ padding: '50px' }}>
+          {this.props.children}
+        </div>
       </Card>
-    )
+    );
   }
 }
 
