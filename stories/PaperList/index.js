ff1d43470688e146bf2563576a51f1439c42b3cb
Zachary Belford <belfordz66@gmail.com>
2018-07-11 10:21:49 -0700

    fixup

--- stories/PaperList/index.js
+++ stories/PaperList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
 import { muiTheme as storyBookMuiTheme } from 'storybook-addon-material-ui';
 
