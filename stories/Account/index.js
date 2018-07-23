2f14a49a9520c61a803a622af30f54ba16e43b82
Zachary Belford <belfordz66@gmail.com>
2018-07-23 09:34:00 +0200

    cleanup cleanup

--- stories/Account/index.js
+++ stories/Account/index.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Account from '../../src/components/Account';
 import { muiTheme as storyBookMuiTheme } from 'storybook-addon-material-ui';
+import Account from '../../src/components/Account';
 import muiTheme from '../../src/theme/index';
 
 storiesOf('Account', module)
   .addDecorator(storyBookMuiTheme(muiTheme))
   .add('default', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       name="account1"
-      onAddressClick={action('address-click')}
-    />))
+    />
+  ))
   .add('editable', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       name="account1"
       editable
-      onAddressClick={action('address-click')}
-    />))
+    />
+  ))
   .add('without Name', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       onAddressClick={action('address-click')}
-    />))
+    />
+  ))
   .add('Identity Icon', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       identity
-    />))
+    />
+  ))
   .add('Identity Icon with name', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       name="account1"
       identity
-    />))
+    />
+  ))
   .add('Identity Icon with editable name', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       editable
       name="account1"
       identity
-    />))
-  .add('Identity Icon with custom size indentity props', () => (
+    />
+  ))
+  .add('Shortened', () => (
     <Account
-      addr="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+      address="0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
       identity
-      identityProps={{ size: 30 }}
-    />));
+      addressWidth="200px"
+    />
+  ));
