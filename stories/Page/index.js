8bc8b01c11c4c9ddda8a42217f285ed5435e2303
Zachary Belford <belfordz66@gmail.com>
2018-08-23 14:51:57 -0700

    remove material-ui for real

--- stories/Page/index.js
+++ stories/Page/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { muiTheme as storyBookMuiTheme } from 'storybook-addon-material-ui';
 import Typography from '@material-ui/core/Typography';
-
-import muiTheme from '../../src/theme/index';
 
 import Page from '../../src/components/Page';
 import Back from '../../src/icons3/Back';
 
 storiesOf('Page', module)
-  .addDecorator(storyBookMuiTheme(muiTheme))
   .add('default', () => (
     <div>
       <Page title="foo bar" leftIcon={<Back />}>
